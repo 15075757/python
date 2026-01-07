41386481c8433caea0b3017918f484de9f10f28f
Yee Peng <tanyeepeng@gmail.com>
2026-01-07 13:55:04 +0800

    added

--- PY/Practise.py
+++ PY/Practise.py
@@ -187,9 +187,11 @@
 
 #lopop control statement
 
-for i in range (1, 11):
-    if i == 2:
-        continue
-    if i == 8:
-        break
-    print(i)
+# for i in range (1, 11):
+#     if i == 2:
+#         continue
+#     if i == 8:
+#         break
+#     print(i)
+
+#nested loop 
